cc895423d5f6aeb2804e68b429044f063cae2f91
blacktyger <blacktyg3r@gmail.com>
2022-11-23 15:21:56 +0000

    first commit

--- giverofepic/giverofepic/urls.py
+++ giverofepic/giverofepic/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
-from epicpy import Wallet, Node
 
 
 api = NinjaAPI()
@@ -9,14 +8,7 @@ api = NinjaAPI()
 
 @api.get("/add")
 def add(request):
-    wallet = Wallet(secret=r'C:\Users\blacktyger\.epic\main\.owner_api_secret')
-    wallet.open_wallet(password='majkut11')
-
-    tx = wallet.send_transaction(method='http', amount=0.077,
-                                 message="https://GiverOfEpic.epic.tech",
-                                 address="http://209.127.178.120:34615")
-
-    return {"result": tx}
+    return
 
 
 urlpatterns = [
