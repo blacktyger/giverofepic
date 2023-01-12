0bd5ce573bd8fff678fe55234aff430a486b5a4f
blacktyger <blacktyg3r@gmail.com>
2023-01-12 20:27:42 +0000

    clean

--- giverofepic/giverofepic/secrets.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-WALLET_PASSWORD = 'majkut11'
-
-if 'windows' in platform.system().lower():
-    WALLET_DIR = r"C:\Users\blacktyger\.epic\main"
-else:
-    WALLET_DIR = "/home/blacktyger/.epic/main"
